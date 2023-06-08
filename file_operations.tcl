7aeaf5a29a0ef9ec85c4d104bcacd71156e7db73
Michael Wiltshire <michael@thewiltshires.com>
2023-06-08 13:02:26 -0500

    temp file directory

--- file_operations.tcl
+++ file_operations.tcl
@@ -2,4 +2,17 @@ package require fileutil
 
 #File Manipulation script
 
+# Copy a file
+file copy "path/to/source/file.txt" "path/to/destination/file.txt"
 
+# Rename a file
+file rename "path/to/old/file.txt" "path/to/new/file.txt"
+
+# Delete a file
+file delete "path/to/file.txt"
+
+# Search for files
+set files [glob "path/to/directory/*.txt"]
+foreach file $files {
+    puts "Found file: $file"
+}
